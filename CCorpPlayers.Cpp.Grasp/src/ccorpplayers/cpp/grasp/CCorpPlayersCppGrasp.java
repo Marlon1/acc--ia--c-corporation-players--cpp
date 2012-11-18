690969a4e70fd7fb9c721b72f49a960a3d8fcaaf
gvillarg <gvillarg@pucp.pe>
2012-11-18 15:11:34 +0000

    GuiaRemison

--- CCorpPlayers.Cpp.Grasp/src/ccorpplayers/cpp/grasp/CCorpPlayersCppGrasp.java
+++ CCorpPlayers.Cpp.Grasp/src/ccorpplayers/cpp/grasp/CCorpPlayersCppGrasp.java
@@ -13,7 +13,15 @@ public class CCorpPlayersCppGrasp {
     /**
      * @param args the command line arguments
      */
+    @SuppressWarnings("empty-statement")
     public static void main(String[] args) {
         // TODO code application logic here
+        String cad;
+        cad = "Hola mundo";
+        int i;
+        for(i=0;cad.charAt(i)!=' ';i++);
+        System.out.println(i);
+        cad=cad.substring(0, i);
+        System.out.println(cad);
     }
 }
