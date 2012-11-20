29ad0f52e687fdb0e4bdae09807dd8c8fe44ac1d
gvillarg <gvillarg@pucp.pe>
2012-11-20 02:20:55 +0000

    Creacion de Algoritmo base

--- CCorpPlayers.Cpp.Grasp/src/Gestores/GRASP.java
+++ CCorpPlayers.Cpp.Grasp/src/Gestores/GRASP.java
@@ -137,4 +137,22 @@ public class GRASP {
         //return  
         return candidatos.get(i);
     }
+    public void ejecutar() {
+        double alfa=0.7;
+        Solucion mejor_sol=null;
+        for (int k = 0; k < 3000; k++) {
+            Lector.leerArchivo();
+            GRASP grasp = new GRASP();
+            Solucion sol = grasp.resolver(Lector.getV(), Lector.getA(), Lector.getVerticeInicial(), alfa);
+            if(sol.getTiempo_total()>=0.001){
+                sol.imprimir();
+                mejor_sol=Solucion.mejorSolucion(mejor_sol,sol);
+            }
+        }
+        System.out.println();
+        Lector.leerArchivo();
+        Lector.imprimirV();
+        System.out.println("Mejor Solucion (alfa: "+alfa+")");
+        mejor_sol.imprimir();
+    }
 }
